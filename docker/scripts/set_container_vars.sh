496d1511883242a03d51dc9d271a23d9624e3cb1
Nick L <iamn1ck@yahoo.com>
2025-11-18 00:32:38 +0100

    add dependencies to dockerfile build a push new docker files fix workflow build

--- docker/scripts/set_container_vars.sh
+++ docker/scripts/set_container_vars.sh
@@ -2,9 +2,9 @@
 
 ###############################################################################
 
-IMAGE_NAME="android-builder-soh"
+IMAGE_NAME="android-openxr-builder-soh"
 IMAGE_TAG="v1.0.0"
-REGISTRY_REPO="jriosbyte/${IMAGE_NAME}"
+REGISTRY_REPO="iamn1ck/${IMAGE_NAME}"
 
 export IMAGE_NAME IMAGE_TAG REGISTRY_REPO
 
