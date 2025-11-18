e66cbc0eab17663e342950b5fcc6ba0ebcf07168
Nick L <iamn1ck@yahoo.com>
2025-11-18 10:08:15 +0100

    fix build

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -8,8 +8,8 @@ set -e
 ###############################################################################
 
 # Setup Android SDK and NDK paths in project local.properties
-echo "sdk.dir=${ANDROID_SDK_ROOT}" > /workspace/local.properties
-echo "ndk.dir=${ANDROID_NDK_HOME}" >> /workspace/local.properties
+echo "sdk.dir=${ANDROID_SDK_ROOT}" > /workspace/Shipwright-android/local.properties
+echo "ndk.dir=${ANDROID_NDK_HOME}" >> /workspace/Shipwright-android/local.properties
 
 # Execute any received command
 exec "$@"
