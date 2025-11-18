5ac727c3db67be819e41f70da1370636af12ef24
Nick L <iamn1ck@yahoo.com>
2025-11-18 10:20:27 +0100

    fix build

--- docker/scripts/build_android_project.sh
+++ docker/scripts/build_android_project.sh
@@ -48,7 +48,7 @@ docker run --network="host" --rm \
     -v "$PROJECT_PATH":/workspace/Shipwright-android \
     -w /workspace/Shipwright-android \
     $IMAGE_NAME \
-    bash -c "cp -a local.properties Android/ && cd Android && ./gradlew ${GRADLEW_BUILD_TYPE}"
+    bash -c "cp -a /workspace/local.properties Android/ && cd Android && ./gradlew ${GRADLEW_BUILD_TYPE}"
 
 BUILD_RESULT=$?
 
